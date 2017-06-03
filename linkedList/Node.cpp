3da5c105c3cd62f78fc91f66adb523804ce716f1
m1cl <michael_ajala@yahoo.de>
2017-06-03 19:10:55 +0200

    add node class and header

---
+++ linkedList/Node.cpp
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Node.cpp
+ *
+ *    Description:  Node Header 
+ *
+ *        Version:  1.0
+ *        Created:  03.06.2017 19:01:23
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Michael Ajala (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "Node.h"
+
+Node::Node(int data){
+  this->data = data;
+}
+
+void Node::setData(int data){
+  this->data = data;
+}
+int Node::getData(){
+  return this->data;
+}
---
+++ linkedList/Node.h
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Node.cpp
+ *
+ *    Description:  Node Class
+ *
+ *        Version:  1.0
+ *        Created:  03.06.2017 18:56:15
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Michael Ajala (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+#ifndef NODE_H
+#define NODE_H
+
+class Node
+{
+private:
+  int data;
+
+public:
+  Node(int);
+  int getData();
+  void setData(int);
+  virtual ~Node();
+};
+
+#endif /* NODE_H */
